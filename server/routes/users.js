a1312a5ac147e177753a723cb30d93d972ffbe0f
Ray Romero <chinoromero213@gmail.com>
2021-03-08 14:13:20 -0800

    bcrypt hash/salt logic added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,47 @@
 const { json } = require('express');
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const validateRegisterInput = require('../validation/register');
 const validateLoginInput = require('../validation/login');
 
-const register = (req, res) => {
+const testUser = {
+  name: 'Ray',
+  email: '1@gmail.com',
+  password: '$2b$10$JCRhDoZVJbsDoWSZVN9gUuT4oL7wWF5JP0FSrTN5NIzBdRvbi3dgm',
+};
+
+const register = async (req, res) => {
   const { email, name, password } = req.body;
   const { errors, isValid } = validateRegisterInput(req.body);
+  const saltRounds = 10;
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  //  TODO:
-  // 2: Register logic to DB
+  try {
+    //  TODO:
+    // 1. Validation check in DB if user exists
 
-  return res
-    .status(201)
-    .json({ status: 'success', data: { user: { email, name } } });
+    // 2. hash password
+    const createdHash = await bcrypt.hash(password, saltRounds, (err, hash) => {
+      // Store hash in DB
+      console.log(hash);
+    });
+
+    // 3. Logic to create user
+    return res
+      .status(201)
+      .json({ status: 'success', data: { user: { email, name } } });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   const { email, password } = req.body;
   const { errors, isValid } = validateLoginInput(req.body);
 
@@ -29,11 +49,20 @@ const login = (req, res) => {
     return res.status(400).json(errors);
   }
 
-  // TODO:
-  // 1. Login logic
-  // 2. Verify Username & password
+  try {
+    // TODO:
+    // 1. Validate if user exists
 
-  return res.json({ message: 'Login connected' });
+    // 2. Compare hash password - using user['password'] to match password
+    const passwordMatches = await bcrypt.compare(password, testUser.password);
+    if (!passwordMatches) {
+      return res.status(401).json({ password: 'Incorrect password' });
+    }
+
+    return res.json({ status: 'success', data: { user: { email } } });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 router.post('/register', register);
