e52c810e26a78448f0b6e000ab466eebf6e1b201
Shay Brown <fire328@ymail.com>
2021-03-09 17:32:03 -0500

    add routers for signup and signin login pages; add styles;

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
 
 import "./App.css";
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+		<MuiThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Route exact path='/' render={() => <Redirect to='/signup' />} />
+				<Route path='/signup' render={() => <Signup />} />
+				<Route path='/signin' render={() => <Signin />} />
+			</BrowserRouter>
+		</MuiThemeProvider>
+	);
 }
 
 export default App;
