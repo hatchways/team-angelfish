26ed7d4acae97a984b6f029bb210394bc16643b9
gbudjeakp <gbudjeakp@gmail.com>
2021-03-10 11:36:43 -0600

    removed unused variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from './component/Header/Header'
 import Flights from './pages/Flights/Flights'
 import Hotels from './pages/Hotels/Hotels'
