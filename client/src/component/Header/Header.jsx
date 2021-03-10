bac3859d9a16c83dc0463700e75250fb62729e1c
gbudjeakp <gbudjeakp@gmail.com>
2021-03-10 03:01:21 -0600

    fixing UI

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,154 +1,71 @@
 import React, {useState} from 'react'
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import AppBar from '@material-ui/core/AppBar';
-import Avatar from '@material-ui/core/Avatar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import {AppBar, Avatar, Toolbar, IconButton, Menu, Typography, MenuItem, Button} from '@material-ui/core'
+
+
 import useStyles from './style'
 
 function Header() {
     const classes = useStyles();
-  const [loggedIn, setLoggedIn] = useState(true)
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+   const [auth, setAuth] = useState(false);
+   const [anchorEl, setAnchorEl] = useState(null);
+   const open = Boolean(anchorEl);
 
-  const handleProfileMenuOpen = (event) => {
+  
+  const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
+  const handleClose = () => {
     setAnchorEl(null);
-    handleMobileMenuClose();
   };
+
   
-  const handleLogOut = () =>{
-    handleMenuClose();
-    setLoggedIn(false)
-  }
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-    {!loggedIn ? <div> <MenuItem onClick={()=> setLoggedIn(true)}>Login</MenuItem></div> :  <div><MenuItem onClick={handleLogOut}>Logout</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem></div>}
-     
-
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <p>Explore</p>
-      </MenuItem>
-
-      <MenuItem>
-        <p>Flights</p>
-      </MenuItem>
-
-      <MenuItem>
-        <p>Hotel</p>
-      </MenuItem>
-
-      <MenuItem>
-        <p>Rent</p>
-      </MenuItem>
-
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <Avatar className={classes.avatar} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU"/> 
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
 
     return (
-        <div className={classes.grow}>
+        <div className={classes.root}>
         <AppBar position="static" className={classes.appbar}>
           <Toolbar>
-            <Typography className={classes.title} variant="h6" noWrap>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
               Travel Booking
             </Typography>
-            
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-            
-             <div className={classes.menuButton}>
-             <p>Explore</p>
-              <p>Flights</p>
-              <p>Hotel</p>
-              <p>Rent</p>
-             </div>
-           
-              <IconButton
-                edge="end"
-                aria-label="account of current user" 
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <Avatar className={classes.avatar} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU"/>
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton
-                aria-label="Mobile List"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-            </div>
+            {auth ? (
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <Avatar className={classes.avatar} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU" onClick={handleMenu}/>
+
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
+              </div>
+            ) : <Button className={classes.loginbtn}>Login</Button>}
           </Toolbar>
         </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
       </div>
     )
 }
--- client/src/component/Header/style.js
+++ client/src/component/Header/style.js
@@ -1,42 +1,24 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    grow: {
+    root: {
       flexGrow: 1,
     },
     appbar:{
-      backgroundColor: 'white'
+      backgroundColor: 'white',
     },
     menuButton: {
       marginRight: theme.spacing(2),
-      display: 'flex',
-      color: 'black',
-      justifyContent: 'space -around',
     },
     title: {
-      color: 'blue',
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
+      flexGrow: 1,
+      color: '#6464FF'
     },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-      },
-    },
-    sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
-      },
-    },
-    pages:{
-        display: 'flex',
-        color: 'black',
-        justifyContent: 'space -around',
-    },
+    loginbtn:{
+      backgroundColor: '#FFA000',
+      color: 'white',
+      marginRight: theme.spacing(2),
+    }
   }));
 
 export default useStyles
