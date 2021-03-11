c8af82918274c8deabe05e5a9942e9a6732c21c1
Shay Brown <fire328@ymail.com>
2021-03-11 16:57:26 -0500

    add route for cities;

--- server/routes/flight.js
+++ server/routes/flight.js
@@ -5,91 +5,9 @@ const router = express.Router();
 const unirest = require("unirest");
 const rapidApiKey = process.env.RAPID_API_KEY;
 const rapidApiHost = process.env.RAPID_API_HOST;
+const validator = require("validator");
 
-//Get All available currencies
-//Example: /api/flights/currencies
-// router.get("/currencies", (req, res, next) => {
-// 	unirest
-// 		.get(
-// 			"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies"
-// 		)
-// 		.headers({
-// 			"x-rapidapi-key": rapidApiKey,
-// 			"x-rapidapi-host": rapidApiHost,
-// 			useQueryString: true,
-// 		})
-// 		.then((response) => {
-// 			res.send(response.body);
-// 		})
-// 		.catch((err) => {
-// 			next(err);
-// 		});
-// });
-
-// //Get All available places
-// //Example : /api/flights/places/france/fr/eur/fr.eu/
-// router.get(
-// 	"/places/:country/:countryCode/:currency/:locale",
-// 	(req, res, next) => {
-// 		const { country, countryCode, currency, locale } = req.params;
-// 		unirest
-// 			.get(
-// 				`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/${countryCode}/${currency}/${locale}/`
-// 			)
-// 			.headers({
-// 				"x-rapidapi-key": rapidApiKey,
-// 				"x-rapidapi-host": rapidApiHost,
-// 				useQueryString: true,
-// 			})
-// 			.query({
-// 				query: country,
-// 			})
-// 			.then((response) => {
-// 				res.send(response.body);
-// 			})
-// 			.catch((err) => {
-// 				next(err);
-// 			});
-// 	}
-// );
-
-// //Retrieve the cheapest quotes from our cache prices.
-// //Example : /api/flights/quotes/US/USD/en-US/SFO-sky/JFK-sky/2021-03-12
-// //Example : /api/flights/quotes/US/USD/en-US/SFO-sky/JFK-sky/2021-03-12?inboundPartialDate=2021-03-21
-// router.get(
-// 	"/quotes/:marketCountry/:currency/:local/:originPlace/:destinationPlace/:outboundPartialDate",
-// 	(req, res, next) => {
-// 		const {
-// 			marketCountry,
-// 			currency,
-// 			locale,
-// 			originPlace,
-// 			destinationPlace,
-// 			outboundPartialDate,
-// 		} = req.params;
-// 		const request = unirest.get(
-// 			`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/${marketCountry}/${currency}/${locale}/${originPlace}/${destinationPlace}/${outboundPartialDate}`
-// 		);
-// 		request.headers({
-// 			"x-rapidapi-key": rapidApiKey,
-// 			"x-rapidapi-host": rapidApiHost,
-// 			useQueryString: true,
-// 		});
-// 		//@Note: There is no validation on inboundPartialDate for this endpoint
-// 		if (req.query.inboundPartialDate) {
-// 			request.query({
-// 				query: req.query.inboundPartialDate,
-// 			});
-// 		}
-// 		request
-// 			.then((response) => {
-// 				res.send(response.body);
-// 			})
-// 			.catch((err) => {
-// 				next(err);
-// 			});
-// 	}
-// );
+//rapid API appears to limit all results up to 10
 
 const dateInPast = (date1, date2 = new Date()) => {
 	if (typeof date2 === "string") {
@@ -140,7 +58,7 @@ router.get("/quotes/:from/:to/:outboundDate", (req, res, next) => {
 			"x-rapidapi-host": rapidApiHost,
 			useQueryString: true,
 		});
-
+		// API does not return results for inboundDate
 		if (inboundDate) {
 			const returnDate = inDate
 				? !dateInPast(inDate, outDate)
@@ -164,5 +82,39 @@ router.get("/quotes/:from/:to/:outboundDate", (req, res, next) => {
 	}
 });
 
+router.get("/places/:regionId", (req, res, next) => {
+	const { regionId } = req.params;
+	// regionId can be either the full name or abbreviation of the state, same results
+	const error = {};
+	const region = validator.isAlpha(regionId)
+		? regionId
+		: (error.region = "Region not in proper format.");
+
+	if (Object.keys(error).length === 0) {
+		const request = unirest(
+			"GET",
+			"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
+		);
+
+		request.query({
+			query: region,
+		});
+
+		request.headers({
+			"x-rapidapi-key": rapidApiKey,
+			"x-rapidapi-host": rapidApiHost,
+			useQueryString: true,
+		});
+		request
+			.then((response) => {
+				res.json(response.body);
+			})
+			.catch((err) => {
+				next(err);
+			});
+	} else {
+		res.send(error);
+	}
+});
 
 module.exports = router;
