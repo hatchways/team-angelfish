8bea6dacc6638564abdd222150e930f4bcbe0c19
Ray Romero <chinoromero213@gmail.com>
2021-03-10 23:04:23 -0800

    Fixed review comments

--- client/src/component/FlightSearchComponent/FlightSearch.js
+++ client/src/component/FlightSearchComponent/FlightSearch.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Grid, Paper, TextField, Button, InputLabel } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
+import { Grid, Paper, TextField, Button, InputLabel } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
 // Mock Data
 const cities = [
-  { title: 'Vancouver' },
-  { title: 'Calgary' },
-  { title: 'Toronto' },
-  { title: 'Bangkok' },
+  { title: "Vancouver" },
+  { title: "Calgary" },
+  { title: "Toronto" },
+  { title: "Bangkok" },
 ];
 
 const FlightSearch = () => {
@@ -20,34 +20,11 @@ const FlightSearch = () => {
   curr.setDate(curr.getDate());
   const date = curr.toISOString().substr(0, 10);
 
-  const [from, setFrom] = useState('Vancouver');
-  const [to, setTo] = useState('Bangkok');
+  const [from, setFrom] = useState("Vancouver");
+  const [to, setTo] = useState("Bangkok");
   const [arrival, setArrival] = useState(date);
   const [departure, setDeparture] = useState(date);
   const [travellers, setTravellers] = useState(1);
-
-  const onSelectFrom = (...[, value]) => {
-    setFrom(value);
-  };
-
-  const onSelectTo = (...[, value]) => {
-    setTo(value);
-  };
-
-  const handleArrival = (e) => {
-    const { value } = e.target;
-    setArrival(value);
-  };
-
-  const handleDeparture = (e) => {
-    const { value } = e.target;
-    setDeparture(value);
-  };
-
-  const handleTravellers = (e) => {
-    const { value } = e.target;
-    setTravellers(value);
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -68,38 +45,38 @@ const FlightSearch = () => {
             <InputLabel>From</InputLabel>
             <Autocomplete
               freeSolo
-              id='from'
-              name='from'
+              id="from"
+              name="from"
               options={cities.map((city) => city.title)}
               defaultValue={from}
               value={from}
-              onChange={onSelectFrom}
+              onChange={(...[, v]) => setFrom(v)}
               style={{ width: 150 }}
-              renderInput={(params) => <TextField name='from' {...params} />}
+              renderInput={(params) => <TextField name="from" {...params} />}
             />
           </Grid>
           <Grid className={classes.input} lg={2} sm={3} xs={6} item>
             <InputLabel>Where to go</InputLabel>
             <Autocomplete
               freeSolo
-              id='to'
-              name='to'
+              id="to"
+              name="to"
               options={cities.map((city) => city.title)}
               defaultValue={to}
               value={to}
-              onChange={onSelectTo}
+              onChange={(...[, v]) => setTo(v)}
               style={{ width: 150 }}
-              renderInput={(params) => <TextField name='to' {...params} />}
+              renderInput={(params) => <TextField name="to" {...params} />}
             />
           </Grid>
           <Grid className={classes.input} lg={2} sm={3} xs={6} item>
             <InputLabel>Arrival</InputLabel>
             <TextField
-              id='date'
-              type='date'
-              name='departure'
+              id="date"
+              type="date"
+              name="departure"
               defaultValue={arrival}
-              onChange={handleArrival}
+              onChange={(e) => setArrival(e.target.value)}
               className={classes.textField}
               InputLabelProps={{
                 shrink: true,
@@ -109,12 +86,12 @@ const FlightSearch = () => {
           <Grid className={classes.input} lg={2} sm={3} xs={6} item>
             <InputLabel>Departure</InputLabel>
             <TextField
-              id='date'
-              type='date'
-              name='departure'
+              id="date"
+              type="date"
+              name="departure"
               defaultValue={departure}
               className={classes.textField}
-              onChange={handleDeparture}
+              onChange={(e) => setDeparture(e.target.value)}
               InputLabelProps={{
                 shrink: true,
               }}
@@ -124,10 +101,10 @@ const FlightSearch = () => {
             <Grid item lg={8} sm={3} xs={6}>
               <InputLabel>Travellers</InputLabel>
               <TextField
-                id='travellers'
-                onChange={handleTravellers}
-                type='number'
-                name='travellers'
+                id="travellers"
+                onChange={(e) => setTravellers(e.target.value)}
+                type="number"
+                name="travellers"
                 value={travellers}
               />
             </Grid>
