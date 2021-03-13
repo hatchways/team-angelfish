9d6386398332f22c88a1a9a51f63f20cd516f9fa
Ray Romero <chinoromero213@gmail.com>
2021-03-13 10:42:15 -0800

    styling fixed on flights page

--- client/src/styles/Flights.js
+++ client/src/styles/Flights.js
@@ -1,52 +1,52 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   root: {
     height: "92%",
-    backgroundColor: '#ededed',
+    backgroundColor: "#ededed",
   },
   container: {
-    height: '75%',
+    height: "75%",
   },
   titleContainer: {
-    height: '100%', 
-    backgroundColor: '#fff',
-    display: 'flex',
-    justifyContent: 'center',
-    textAlign: 'center',
-    paddingTop: '100px',
-    '@media (max-width:1100px)': {
-      paddingTop: '100px',
+    height: "100%",
+    backgroundColor: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingTop: "100px",
+    "@media (max-width:1100px)": {
+      paddingTop: "100px",
     },
   },
   header: {
-    padding: '10px',
-    fontSize: '3.5rem',
-    '@media (max-width:1100px)': {
-      fontSize: '2.8rem',
-      paddingTop: '100px',
+    padding: "10px",
+    fontSize: "3.5rem",
+    "@media (max-width:1100px)": {
+      fontSize: "2.8rem",
+      paddingTop: "100px",
     },
-    '@media (max-width:600px)': {
-      fontSize: '2rem',
-      paddingTop: '100px',
+    "@media (max-width:600px)": {
+      fontSize: "2rem",
+      paddingTop: "100px",
     },
   },
   heroContainer: {
-    height: '100%',
+    height: "100%",
   },
   heroImg: {
-    height: '100%',
-    objectFit: 'cover',
-    width: '100%',
+    height: "100%",
+    objectFit: "cover",
+    width: "100%",
   },
   searchDiv: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    position: 'absolute',
-    bottom: '150px',
-    '@media (max-width:900px)': {
-      position: 'initial',
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    position: "absolute",
+    bottom: "210px",
+    "@media (max-width:900px)": {
+      position: "initial",
     },
   },
 }));
