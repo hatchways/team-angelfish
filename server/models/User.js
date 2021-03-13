322c2823d870128e751ec4eb776212295d5ae2f3
Ray Romero <chinoromero213@gmail.com>
2021-03-12 21:25:29 -0800

    Customer model added

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+
+const customerSchema = new mongoose.Schema({
+  stripeId: {
+    type: String,
+    required: true,
+  },
+});
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  customer: {
+    type: customerSchema,
+    default: null,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
