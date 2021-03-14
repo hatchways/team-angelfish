65aa75bf653f6711582113c26e87596b0e7254a8
gbudjeakp <gbudjeakp@gmail.com>
2021-03-14 00:48:15 -0600

    created model template

---
+++ server/models/Itinerary.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const itinerarySchema = new Schema({
+  // Add schema structure
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  
+  module.exports = User;
