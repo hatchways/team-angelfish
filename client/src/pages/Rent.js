11d537621774511bf114ed8cbdb6fe74905bae45
tojonirina <tojonirina88@gmail.com>
2021-03-16 10:37:04 +0400

    Add header

--- client/src/pages/Rent.js
+++ client/src/pages/Rent.js
@@ -2,10 +2,35 @@
 
 import React from "react";
 
+import { Grid, Typography } from "@material-ui/core";
+
+import backgroundImg from "../assets/images/car-rental.jpg";
+import FlightSearchComponent from "../components/FlightSearch";
+
+import useStyles from "../styles/Flights";
+
 function Rent() {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<h1>This is rent page</h1>
+		<div className={classes.root}>
+			<Grid classes={{container: classes.container}} container>
+				<Grid className={classes.titleContainer} item xs={5}>
+					<Typography className={classes.header}>
+						Book now with full flexibility
+					</Typography>
+				</Grid>
+				<Grid className={classes.heroContainer} item xs={7}>
+					<img
+						className={classes.heroImg}
+						src={backgroundImg}
+						alt="birds eye beach view"
+					/>
+				</Grid>
+			</Grid>
+			<div className={classes.searchDiv}>
+				<FlightSearchComponent />
+			</div>
 		</div>
 	);
 }
---
+++ client/src/styles/Rent.js
@@ -0,0 +1,53 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  root: {
+    height: "92%",
+    backgroundColor: "#ededed",
+  },
+  container: {
+    height: "75%",
+  },
+  titleContainer: {
+    height: "100%",
+    backgroundColor: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingTop: 100,
+    "@media (max-width:1100px)": {
+      paddingTop: 100,
+    },
+  },
+  header: {
+    padding: 10,
+    fontSize: "3.5rem",
+    "@media (max-width:1100px)": {
+      fontSize: "2.8rem",
+      paddingTop: 100,
+    },
+    "@media (max-width:600px)": {
+      fontSize: "2rem",
+      paddingTop: 100,
+    },
+    color: "red"
+  },
+  heroContainer: {
+    height: "100%",
+  },
+  heroImg: {
+    height: "100%",
+    objectFit: "cover",
+    width: "100%",
+  },
+  searchDiv: {
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    position: "absolute",
+    bottom: 210,
+    "@media (max-width:900px)": {
+      position: "initial",
+    },
+  },
+}));
