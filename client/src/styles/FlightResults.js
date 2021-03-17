d08322d33b626274886fa6f8f5efd3ce62c3218c
Shay Brown <fire328@ymail.com>
2021-03-17 17:17:21 -0400

    add accordion for flight search results with styling and images;

---
+++ client/src/styles/FlightResults.js
@@ -0,0 +1,35 @@
+/** @format */
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	departFlightsContainer: {
+		border: "1px solid #ced4da",
+		margin: 0,
+		width: "100%"
+	},
+	bottomHeader: {
+		fontWeight: theme.typography.fontWeightMedium,
+		fontSize: "1.62em",
+	},
+	subtitle: {
+		color: "#a6a6a6",
+		marginBottom: "1.5em",
+	},
+	divider: {
+		marginBottom: "2em",
+	},
+	flightInfoContainer: {
+		padding: "1em 3.2em 2em 2.8em",
+		width: "100%"
+	},
+	margin: {
+		marginTop: "3em",
+	},
+	formControl: {
+    minWidth: 120,
+	marginBottom: "1em"
+  }
+}));
+
+export default useStyles;
