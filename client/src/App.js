8daa2fd1dbd9aa9fda4890bd655271dfea4460b6
Ray Romero <chinoromero213@gmail.com>
2021-03-18 17:34:06 -0700

    Shuffle functionality implemented

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,11 @@ import Flights from "./pages/Flights";
 import Hotels from "./pages/Hotels";
 import Rent from "./pages/Rent";
 import Header from "./components/Header";
-import Profile from "./pages/Profile";
+// import Profile from "./pages/Profile";
 import Profile from "./pages/User/UserProfile";
 import Checkout from "./components/Stripe/Checkout";
 import Success from "./components/Stripe/Success";
 import Error from "./components/Stripe/Error";
-
 
 import { Provider } from "./context";
 
@@ -21,25 +20,26 @@ import { theme } from "./themes/theme";
 
 import "./App.css";
 
-
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Redirect exact from="/" to="/explore" />
-          {/* @TODO: Add userId props here and pass the currentUserId from Backend */}
-          <Route path="/explore" component={() => <ExplorerPage />} />
-          <Route exact path="/flights" component={Flights} />
-          <Route exact path="/hotel" component={Hotels} />
-          <Route exact path="/rent" component={Rent} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/payment-success" component={Success} />
-          <Route exact path="/payment-error" component={Error} />
-        </Switch>
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Redirect exact from="/" to="/explore" />
+            {/* @TODO: Add userId props here and pass the currentUserId from Backend */}
+            <Route path="/explore" component={() => <ExplorerPage />} />
+            <Route exact path="/flights" component={Flights} />
+            <Route exact path="/hotel" component={Hotels} />
+            <Route exact path="/rent" component={Rent} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/payment-success" component={Success} />
+            <Route exact path="/payment-error" component={Error} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </MuiThemeProvider>
   );
 }
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,21 +1,29 @@
 export const initialState = {
   authenticated: false,
   user: {},
+  loading: true,
 };
 
 export const userReducer = (initialState, action) => {
   switch (action.type) {
+    case "LOADING":
+      return {
+        ...initialState,
+        loading: true,
+      };
     case "AUTHENTICATED":
       return {
         ...initialState,
         authenticated: true,
         user: action.payload.user,
+        loading: false,
       };
     case "UNAUTHENTICATED":
       return {
         ...initialState,
         authenticated: false,
         user: null,
+        loading: false,
       };
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
