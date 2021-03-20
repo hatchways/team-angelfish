f5d88846c7c4c0c0f87f4c9a3a7eb216f0d9ddaa
Ray Romero <chinoromero213@gmail.com>
2021-03-19 19:52:43 -0700

    Delete commented code

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,6 @@ function App() {
           <Header />
           <Switch>
             <Redirect exact from="/" to="/explore" />
-            {/* @TODO: Add userId props here and pass the currentUserId from Backend */}
             <Route path="/explore" component={() => <ExplorerPage />} />
             <Route exact path="/flights" component={Flights} />
             <Route exact path="/hotel" component={Hotels} />
