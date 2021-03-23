d5c7d98adc1a30e45ea5acfcb78076d7aa22c83d
Shay Brown <phoenixburning89@gmail.com>
2021-03-23 10:03:58 -0400

    add logout to reducer;

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,44 +1,48 @@
+/** @format */
+
 export const initialState = {
-  authenticated: false,
-  user: {},
-  cart: { flights: [], hotels: [], rentalCar: [] },
+	authenticated: false,
+	user: {},
+	cart: { flights: [], hotels: [], rentalCar: [] },
 };
 
 export const userReducer = (state = initialState, action) => {
-  console.log(action);
-  switch (action.type) {
-    case "AUTHENTICATED":
-      return {
-        ...state,
-        authenticated: true,
-        user: action.payload.user,
-      };
-    case "UNAUTHENTICATED":
-      return {
-        ...state,
-        authenticated: false,
-        user: null,
-      };
-    case "ADD_TO_CART":
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          [action.purhcaseType]: [
-            ...state.cart[action.purhcaseType],
-            action.item,
-          ],
-        },
-      };
-    case "REMOVE_FROM_CART":
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          [action.purchaseType]: [],
-        },
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+	console.log(action);
+	switch (action.type) {
+		case "AUTHENTICATED":
+			return {
+				...state,
+				authenticated: true,
+				user: action.payload.user,
+			};
+		case "UNAUTHENTICATED":
+			return {
+				...state,
+				authenticated: false,
+				user: null,
+			};
+		case "ADD_TO_CART":
+			return {
+				...state,
+				cart: {
+					...state.cart,
+					[action.purhcaseType]: [
+						...state.cart[action.purhcaseType],
+						action.item,
+					],
+				},
+			};
+		case "REMOVE_FROM_CART":
+			return {
+				...state,
+				cart: {
+					...state.cart,
+					[action.purchaseType]: [],
+				},
+			};
+		case "LOG_OUT":
+			return initialState;
+		default:
+			throw new Error(`Unhandled action type: ${action.type}`);
+	}
 };
