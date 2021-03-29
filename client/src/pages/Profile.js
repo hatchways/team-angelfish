7744407e573e974fc298f6826b58ed8b33194b0c
Shay Brown <phoenixburning89@gmail.com>
2021-03-29 12:14:17 -0400

    add link for trips; make responsive;

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,11 +8,18 @@ import FavoriteDestination from "./FavoriteDestinantions";
 import AccountSettings from "./AccountSettings";
 import { Avatar, Drawer, Typography, Button, Grid } from "@material-ui/core";
 import { useDispatchContext } from "../context";
+import Itinerary from "./Itinerary";
 
 function Profile() {
 	const { path } = useRouteMatch();
 	const dispatch = useDispatchContext();
 	const classes = useStyles();
+	const menuItems = [
+		{ name: "Favorite Destinations", link: "favoritedestinations" },
+		{ name: "Notifications", link: "notifications" },
+		{ name: "Account Settings", link: "accountsettings" },
+		{ name: "My Trips", link: "trips" },
+	];
 	const mockUser = {
 		avatar:
 			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdX7wWCMOvGYD6_4-MthVKf-DjjgLF_GqQzg&usqp=CAU",
@@ -29,58 +36,50 @@ function Profile() {
 	};
 
 	return (
-		<Grid className={classes.root}>
-			<Drawer
-				className={classes.drawer}
-				variant="permanent"
-				classes={{
-					paper: classes.drawerPaper,
-				}}
-			>
-				<Grid className={classes.profilePosition}>
-					<Avatar src={mockUser.avatar} className={classes.avatar} />
-					<Typography variant="h6">{mockUser.name}</Typography>
-					<Typography className={classes.email}>{mockUser.email}</Typography>
-
-					<Grid className={classes.editBtnContainer}>
+		<Grid container className={classes.root}>
+			<Grid item container sm={4} lg={3} className={classes.profileContainer}>
+				<Drawer
+					variant="permanent"
+					classes={{
+						root: classes.drawerRoot,
+						paper: classes.drawerPaper,
+					}}
+				>
+					<Grid item className={classes.profilePosition}>
+						<Avatar src={mockUser.avatar} className={classes.avatar} />
+						<Typography variant="h6" className={classes.avatarInfo}>
+							{mockUser.name}
+						</Typography>
+						<Typography className={classes.avatarInfo}>
+							{mockUser.email}
+						</Typography>
+					</Grid>
+					<Grid item>
 						<Button className={classes.editBtn} variant="outlined">
 							Edit
 						</Button>
 					</Grid>
-				</Grid>
-				<Grid className={classes.drawerContainer}>
-					<ul>
-						<NavLink
-							activeStyle={{ color: "black", borderLeft: "3px solid #FFA000" }}
-							className={classes.profileLinks}
-							to="/profile/favoritedestinations"
-						>
-							<li>Favorite Destinations</li>
-						</NavLink>
-						<NavLink
-							activeStyle={{ color: "black", borderLeft: "3px solid #FFA000" }}
-							className={classes.profileLinks}
-							to="/profile/notifications"
-						>
-							<li>Notifications</li>
-						</NavLink>
-						<NavLink
-							activeStyle={{ color: "black", borderLeft: "3px solid #FFA000" }}
-							className={classes.profileLinks}
-							to="/profile/accountsettings"
-						>
-							<li>Account Settings</li>
-						</NavLink>
-					</ul>
-				</Grid>
-				<Grid className={classes.logoutBtn}>
-					<Button className={classes.logoutMainBtn} onClick={handleLogout}>
-						Logout
-					</Button>
-				</Grid>
-			</Drawer>
-
-			<Grid className={classes.content}>
+					<Grid item>
+						<ul className={classes.linksContainer}>
+							{menuItems.map((item) => (
+								<NavLink
+									key={item.name}
+									className={classes.profileLinks}
+									to={`/profile/${item.link}`}
+								>
+									<li>{item.name}</li>
+								</NavLink>
+							))}
+						</ul>
+					</Grid>
+					<Grid item>
+						<Button className={classes.logoutBtn} onClick={handleLogout}>
+							Log out
+						</Button>
+					</Grid>
+				</Drawer>
+			</Grid>
+			<Grid item sm={8} lg={9} className={classes.tripContainer}>
 				<Switch>
 					<Route
 						path={`${path}/favoritedestinations`}
@@ -88,6 +87,7 @@ function Profile() {
 					/>
 					<Route path={`${path}/notifications`} component={Notifications} />
 					<Route path={`${path}/accountsettings`} component={AccountSettings} />
+					<Route path={`${path}/trips`} component={Itinerary} />
 				</Switch>
 			</Grid>
 		</Grid>
