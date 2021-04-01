ea62062833ad56bfd2f2ef53edcc3f255a8a933e
gbudjeakp <gbudjeakp@gmail.com>
2021-03-31 21:41:38 -0500

    adding dotenv line

--- server/services/file-upload.js
+++ server/services/file-upload.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+
 
 aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
